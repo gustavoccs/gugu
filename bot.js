6d0e0510203881ba819ffe1efc4e12130ade2f46
gustavoccs <32191866+gustavoccs@users.noreply.github.com>
2017-10-25 00:49:24 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,26 +6,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '.ping') {
+    if (message.content === 'ping') {
     	message.reply('pong');
-  	
+  	}
 });
 
-client.on('message', message => {
-    if (message.content === '!on?') {
-        channel = ctx.message.channel
-        t1 = time.perf_counter()
-        await self.bot.send_typing(channel)
-        t2 = time.perf_counter()
-        message.reply(":ping_pong: Ping: {}ms".format(round((t2-t1)*1000)))
-  
-});
-client.on('message', message => {
-  // If the message is "what is my avatar"
-  if (message.content === '.avatar') {
-    // Send the user's avatar URL
-    message.reply(message.author.displayAvatarURL());
-  }
-});
-
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
