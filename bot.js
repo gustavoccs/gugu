a8524654fc24688afedf52aed263de61fa504bfa
gustavoccs <32191866+gustavoccs@users.noreply.github.com>
2017-10-26 23:44:55 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '!chk') {
+    if (message.content === '!chk ') {
     	message.reply('APROVADA ESSA CC SUA');
   	}
 });
