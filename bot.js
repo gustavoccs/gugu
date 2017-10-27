624ec5a74795f17bfd560da6c2eedaf42ea25ba3
gustavoccs <32191866+gustavoccs@users.noreply.github.com>
2017-10-27 00:02:30 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'teste') {
-    	message.reply('testado'+ content);
+    	message.reply("testado" + content);
   	}
 });
 
